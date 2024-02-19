53083f3351cff0ab7625918a9b38afd10b484cb4
wangxuefei <wangxuefei@inspur.com>
2024-02-19 17:14:26 +0800

    create springboot project

---
+++ weblog-springboot/weblog-module-admin/src/test/java/com/wxf/weblog/admin/WeblogModuleAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxf.weblog.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblogModuleAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
